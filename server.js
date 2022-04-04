553a1fbec25786ca0f97094d8a43257f067efa35
Marcin <mistamasta7@gmail.com>
2022-04-04 22:01:41 +0200

    Extend client and server funcionality & add identifiers

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 const express = require('express');
+const socket = require('socket.io');
+
+const port = 8000;
+const tasks = [];
 
 const app = express();
-const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running...');
+
+const server = app.listen(port, () => {
+    console.log(`App running at http://localhost:${port}`);
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('New client! Its id – ' + socket.id);
+    socket.emit('updateData', tasks);
+
+    socket.on('addTask', (task) => {
+        tasks.push(task);
+        socket.broadcast.emit('addTask', task);
+    });
+
+    socket.on('removeTask', (id) => {
+        tasks.filter(task => task.id !== id);
+        socket.broadcast.emit('removeTask', id);
+    });
 });
 
 app.use((req, res) => {
-  res.status(404).send({ message: 'Not found...' });
+    res.status(200).send('code 200, success!');
 });
